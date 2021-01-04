c870b4feecf3a19e1c3aab6a10b0a5c614c1b4ad
dak79 <danielecampari@gmail.com>
2021-01-05 01:54:47 +0300

    adding my first js file

---
+++ js/light.js
@@ -0,0 +1,137 @@
+function lightMode() {
+    var title = document.querySelector("#mainTitle");
+    title.style.color = "black";
+    title.style.backgroundColor = "#F4FF81";
+    title.style.bottomBorder = "5px solid  #FA6F03";
+
+    var h1 = document.querySelector("#firstTitle");
+    h1.style.backgroundColor = "#F4FF81";
+
+    var pres = document.querySelector("#present");
+    pres.style.color = "black";
+    pres.style.backgroundColor = "#F9FBE7";
+
+    var eduTit = document.querySelector("#eduTitle");
+    eduTit.style.color = "black";
+    eduTit.style.backgroundColor = "#F4FF81";
+    eduTit.style.bottomBorder = "5px solid #FA6F03";
+
+    var eduOne = document.querySelector("#eduOne");
+    eduOne.style.color = "black";
+    eduOne.style.backgroundColor = "#F9FBE7";
+
+    var w3c = document.querySelector("#w3c");
+    w3c.style.color = "black";
+    w3c.style.backgroundColor = "#F4FF81";
+
+    var eduTwo = document.querySelector("#eduTwo");
+    eduTwo.style.color = "black";
+    eduTwo.style.backgroundColor = "#F9FBE7";
+
+    var harvard = document.querySelector("#harvard");
+    harvard.style.color = "black";
+    harvard.style.backgroundColor = "#F4FF81";
+
+    var eduThree = document.querySelector("#eduThree");
+    eduThree.style.color = "black";
+    eduThree.style.backgroundColor = "#F9FBE7";
+
+    var siena = document.querySelector("#siena");
+    siena.style.color = "black";
+    siena.style.backgroundColor = "#F4FF81";
+
+    var eduFour = document.querySelector("#eduFour");
+    eduFour.style.color = "black";
+    eduFour.style.backgroundColor = "#F9FBE7";
+
+    var bologna = document.querySelector("#bologna");
+    bologna.style.color = "black";
+    bologna.style.backgroundColor = "#F4FF81";
+
+    var workTit = document.querySelector("#workTitle");
+    workTit.style.color = "black";
+    workTit.style.backgroundColor = "#F4FF81";
+    workTit.style.bottomBorder = "5px solid #FA6F03";
+
+    var workOne = document.querySelector("#workOne");
+    workOne.style.color = "black";
+    workOne.style.backgroundColor = "#F9FBE7";
+
+    var idea = document.querySelector("#idea");
+    idea.style.color = "black";
+    idea.style.backgroundColor = "#F4FF81";
+
+    var workTwo = document.querySelector("#workTwo");
+    workTwo.style.color = "black";
+    workTwo.style.backgroundColor = "#F9FBE7";
+
+    var hacettepe = document.querySelector("#hacettepe");
+    hacettepe.style.color = "black";
+    hacettepe.style.backgroundColor = "#F4FF81";
+
+    var workThree = document.querySelector("#workThree");
+    workThree.style.color = "black";
+    workThree.style.backgroundColor = "#F9FBE7";
+
+    var teacher = document.querySelector("#teacher");
+    teacher.style.color = "black";
+    teacher.style.backgroundColor = "#F4FF81";
+
+    var workFour = document.querySelector("#workFour");
+    workFour.style.color = "black";
+    workFour.style.backgroundColor = "#F9FBE7";
+
+    var dire = document.querySelector("#dire");
+    dire.style.color = "black";
+    dire.style.backgroundColor = "#F4FF81";
+
+    var favTit = document.querySelector("#favTitle");
+    favTit.style.color = "black";
+    favTit.style.backgroundColor = "#F4FF81";
+    favTit.style.bottomBorder = "5px solid #FA6F03";
+
+    var favOne = document.querySelector("#favOne");
+    favOne.style.color = "black";
+    favOne.style.backgroundColor = "#F9FBE7";
+
+    var food = document.querySelector("#food");
+    food.style.color = "black";
+    food.style.backgroundColor = "#F4FF81";
+
+    var favTwo = document.querySelector("#favTwo");
+    favTwo.style.color = "black";
+    favTwo.style.backgroundColor = "#F9FBE7";
+
+    var website = document.querySelector("#website");
+    website.style.color = "black";
+    website.style.backgroundColor = "#F4FF81";
+
+    var liOne = document.querySelector("#liOne");
+    liOne.style.color = "black";
+
+    var liTwo = document.querySelector("#liTwo");
+    liTwo.style.color = "black";
+
+    var liThree = document.querySelector("#liThree");
+    liThree.style.color = "black";
+
+    var endTitle = document.querySelector("#endTitle");
+    endTitle.style.color = "black";
+    endTitle.style.backgroundColor = "#F4FF81";
+
+    var liEndOne = document.querySelector("#liEndOne");
+    liEndOne.style.color = "black";
+
+    var liEndTwo = document.querySelector("#liEndTwo");
+    liEndTwo.style.color = "black";
+
+    var buttonOne = document.querySelector("#buttonOne");
+    buttonOne.style.color = "black";
+    buttonOne.style.backgroundColor = "#F4FF81";
+    buttonOne.style.border = "0.2em solid #FA6F03";
+
+    var buttonOne = document.querySelector("#buttonTwo");
+    buttonTwo.style.color = "black";
+    buttonTwo.style.backgroundColor = "#F4FF81";
+    buttonTwo.style.border = "0.2em solid #FA6F03";
+}
